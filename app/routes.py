1cee15c96f8976f96b5a87853e4e830715e2faca
Timothy Piggott <piggott291@gmail.com>
2020-11-06 02:17:29 -0500

    Building files for client side displaying!

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,14 @@ def first_project():
     return render_template('blog-description.html')
 
 # TODO: Add this path and the path to the other projects
+@app.route('/user-threads')
+def user_threads():
+    return render_template('sut-in-c.html')
+
+@app.route('/rpc-server')
+def rpc_server():
+    return render_template('rpc-in-c.html')
+
+@app.route('/hosting-error')
+def hosting_error():
+    return render_template('not-hosting.html')
