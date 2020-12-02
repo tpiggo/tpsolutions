1b67ad98468a7ad32a43dfe04b7dfa833423598f
tpiggo <49927579+tpiggo@users.noreply.github.com>
2020-12-02 18:45:46 -0500

    Fixing routes and adding content

--- app/routes.py
+++ app/routes.py
@@ -12,17 +12,17 @@ def home():
 
 @app.route('/basic-blog')
 def first_project():
-    return render_template('blog-description.html')
+    return render_template('blog-description.html', description="Simple blog image capituring beauty!")
 
 # TODO: Add this path and the path to the other projects
 @app.route('/user-threads')
 def user_threads():
-    return render_template('sut-in-c.html')
+    return render_template('sut-in-c.html', description="Simple User threads diagram.")
 
 @app.route('/rpc-server')
 def rpc_server():
-    return render_template('rpc-in-c.html')
+    return render_template('rpc-in-c.html', description="Remote procedre calls terminal.")
 
 @app.route('/hosting-error')
 def hosting_error():
-    return render_template('not-hosting.html')
+    return render_template('not-hosting.html', description="Sorry not hosting this project currently!")
